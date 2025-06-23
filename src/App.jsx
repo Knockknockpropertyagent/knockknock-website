37c2cea2d3940bd98f0e2b53a3259ef87fd9fdce
Knockknockpropertyagent <knockknockpropertyagent@gmail.com>
2025-06-23 14:49:52 +0100

    Update website layout

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="text-white bg-[#384731] min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-serif">Find your new home</h1>
+      <p className="mt-2 text-lg">We help you buy, sell, rent and manage property with trust and care.</p>
+    </div>
+  )
+}
+
+export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#384731'
+      },
+      fontFamily: {
+        serif: ['Georgia', 'serif']
+      }
+    },
   },
   plugins: [],
-};
+}
